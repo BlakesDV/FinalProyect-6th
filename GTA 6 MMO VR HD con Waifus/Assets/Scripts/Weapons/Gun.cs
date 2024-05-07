631de70ac40352c831bdcb9700551e0bf002a47c
BlakesDV <dmunoz@cocoschool.mx>
2024-05-07 09:11:28 -0600

    Interactables commit

---
+++ GTA 6 MMO VR HD con Waifus/Assets/Scripts/Weapons/Gun.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform bulletSpawner;
+    [SerializeField] float bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable _interactor = GetComponent<XRGrabInteractable>();
+        _interactor.activated.AddListener(FireGun);
+    }
+
+    public void FireGun(ActivateEventArgs arg) {
+        GameObject tempBullet = Instantiate(bullet, bulletSpawner.position, Quaternion.identity);
+        tempBullet.GetComponent<Rigidbody>().velocity = bulletSpawner.forward * bulletSpeed;
+        Destroy(tempBullet, 6);
+    }
+}
