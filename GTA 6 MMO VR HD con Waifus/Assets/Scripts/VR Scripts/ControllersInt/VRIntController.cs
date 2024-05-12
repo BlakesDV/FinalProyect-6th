50c8118ee8f7e8893c019358e0803bc54a75ea68
BlakesDV <dmunoz@cocoschool.mx>
2024-05-12 14:23:25 -0600

    Interactables setting
    Interavtables set up for puzzle

---
+++ GTA 6 MMO VR HD con Waifus/Assets/Scripts/VR Scripts/ControllersInt/VRIntController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class VRIntController : MonoBehaviour
+{
+    float sliderValue;
+    XRSlider xRSlider;
+    public GameObject key;
+    public GameObject socket;
+
+    public GameObject cannonball;
+    public Transform cannonPos;
+    public Rigidbody cannon;
+    public float cannonballSpeed = 10f;
+    XRSocketInteractor sx;
+
+    void Start()
+    {
+        xRSlider = GetComponent<XRSlider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MovX()
+    {
+        print("Movimiento en X");
+    }
+
+    public void MovY()
+    {
+        print("Movimiento en Y");
+    }
+
+    public void TriggerEnter()
+    {
+        Instantiate(cannonball, cannonPos.position, Quaternion.identity);
+
+        IXRSelectInteractable x = sx.GetOldestInteractableSelected();
+        Destroy(x.transform.gameObject);
+
+    }
+
+    public void ValueChange()
+    {
+        
+    }
+
+    public void ButtonPress()
+    {
+
+    }
+
+    public void KeySpawner()
+    {
+        Instantiate(key, transform.position, Quaternion.identity);
+    }
+
+    public void LeverOn()
+    {
+        socket.SetActive(false);
+    }
+
+    public void LeverOff()
+    {
+        socket.SetActive(true);
+    }
+}
